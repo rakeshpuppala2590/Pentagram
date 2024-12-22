9396104ce0f399d8c57f5710077f3ec221098be0
Rakesh Puppala <34569209+rakeshpuppala2590@users.noreply.github.com>
2024-12-21 23:13:32 -0800

    fixed prod

--- src/app/api/images/[id]/route.ts
+++ src/app/api/images/[id]/route.ts
@@ -1,11 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../../lib/prisma";
 
-export async function POST(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
-  const { id } = params;
+interface Context {
+  params: {
+    id: string;
+  };
+}
+
+export async function POST(request: NextRequest, context: Context) {
+  const { id } = context.params;
   const { action, comment } = await request.json();
 
   switch (action) {
